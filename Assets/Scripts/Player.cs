22129b49b79815865346ab165ee4aa9365370ee2
nominal251 <jackhardy524@gmail.com>
2025-03-30 20:03:37 -0400

    Update bottom screen boundary

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour
     private float verticalInput;
 
     private float horizontalScreenLimit = 9.5f;
-    private float verticalScreenLimit = 6.5f;
+    private float verticalScreenLimit = 3.5f;
 
     public GameObject bulletPrefab;
 
